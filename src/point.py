99d0211d8a8b76a7e9af39d48811846bd30312ef
Pierre Petitbon <pierre.petitbon@ensimag.grenoble-inp.fr>
2015-02-10 00:52:43 +0100

    modif formatage affichage points

--- src/point.py
+++ src/point.py
@@ -3,14 +3,14 @@ class Point2D:
         self.x = x
         self.y = y
     def __str__(self):
-        return "x = {}, y = {}".format(str(self.x), str(self.y))
+        return "({}, {})".format(str(self.x), str(self.y))
 
 class Point3D(Point2D):
     def __init__(self, x = 0.0, y = 0.0, z = 0.0):
         Point2D.__init__(self, x, y)
         self.z = z
     def __str__(self):
-        return Point2D.__str__(self) + ", z = {}".format(str(self.z))
+        return Point2D.__str__(self)[:-1] + ", {})".format(str(self.z))
 
 class Segment:
     def __init__(self, p1, p2):
