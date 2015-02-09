24ae87c7cd9218af0f57952e79c32dd1ad411340
Pierre Petitbon <pierre.petitbon@ensimag.grenoble-inp.fr>
2015-02-10 00:26:30 +0100

    ajout classes Segment et Triangle (constructeurs)

--- src/point.py
+++ src/point.py
@@ -7,3 +7,13 @@ class Point3D(Point2D):
     def __init__(self, x = 0, y = 0, z = 0):
         Point2D.__init__(self, x, y)
         self.z = z
+
+class Segment:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+class Triangle(Segment):
+    def __init__(self, p1, p2, p3):
+        Segment.__init__(self, p1, p2)
+        self.p3 = p3
