90a9aeb6d21e06b0bf8685bb1dc6207c96a27c38
Pierre Petitbon <pierre.petitbon@ensimag.grenoble-inp.fr>
2015-02-10 00:16:37 +0100

    test constructeurs Point2D, Point3D

---
+++ src/testPoint.py
@@ -0,0 +1,10 @@
+import point
+
+p2 = point.Point2D(1,2)
+print("x =", p2.x, "y =", p2.y)
+p3 = point.Point3D(1,2,3)
+print("x =", p3.x, "y =", p3.y, "z =", p3.z)
+p2 = point.Point2D();
+print("x =", p2.x, "y =", p2.y)
+p3 = point.Point3D();
+print("x =", p3.x, "y =", p3.y, "z =", p3.z)
