4a67ee844466998e9b7fc0e7f47d54f9f8cd3bc8
Pierre Petitbon <pierre.petitbon@ensimag.grenoble-inp.fr>
2015-02-10 00:40:19 +0100

    ajout méthodes str pour points

--- src/point.py
+++ src/point.py
@@ -2,11 +2,15 @@ class Point2D:
     def __init__(self, x = 0, y = 0):
         self.x = x
         self.y = y
+    def __str__(self):
+        return "x = {}, y = {}".format(str(self.x), str(self.y))
 
 class Point3D(Point2D):
     def __init__(self, x = 0, y = 0, z = 0):
         Point2D.__init__(self, x, y)
         self.z = z
+    def __str__(self):
+        return Point2D.__str__(self) + ", z = {}".format(str(self.z))
 
 class Segment:
     def __init__(self, p1, p2):
