b579dd2fb361e5f883be05adf8d5c9a9379c1e7b
Pierre Petitbon <Pierre.Petitbon@ensimag.grenoble-inp.fr>
2015-02-10 16:48:07 +0100

    suppression héritage segment -> triangle

--- src/jimn/triangle.py
+++ src/jimn/triangle.py
@@ -1,9 +1,8 @@
 from jimn.point import *
 from jimn.segment import *
 
-class triangle(segment):
+class triangle:
     def __init__(self, p1 = point(), p2 = point(), p3 = point()):
-        segment.__init__(self, p1, p2)
-        self.sommets.append(p3)
+        self.sommets = [p1, p2, p3]
     def __str__(self):
-        return segment.__str__(self)[:-1] + " ; {}]".format(str(self.sommets[2]))
+        return  "[{} ; {} ; {}]".format(str(self.sommets[0]), str(self.sommets[1]), str(self.sommets[2]))
