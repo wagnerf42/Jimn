088c7dbadac60d4bb8237a338d65db45af845b56
Pierre Petitbon <Pierre.Petitbon@ensimag.grenoble-inp.fr>
2015-02-10 14:51:56 +0100

    modif camelcase en underscores

--- src/point.py
+++ src/point.py
@@ -1,55 +1,55 @@
-class Point2D:
+class point2d:
     def __init__(self, x = 0.0, y = 0.0):
         self.x = x
         self.y = y
     def __str__(self):
         return "({}, {})".format(str(self.x), str(self.y))
 
-class Point3D(Point2D):
+class point3d(point2d):
     def __init__(self, x = 0.0, y = 0.0, z = 0.0):
-        Point2D.__init__(self, x, y)
+        point2d.__init__(self, x, y)
         self.z = z
     def __str__(self):
-        return Point2D.__str__(self)[:-1] + ", {})".format(str(self.z))
+        return point2d.__str__(self)[:-1] + ", {})".format(str(self.z))
 
-class Segment:
+class segment:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
     def __str__(self):
         return "[{} ; {}]".format(str(self.p1), str(self.p2))
 
-class Triangle(Segment):
-    def __init__(self, p1 = Point3D(), p2 = Point3D(), p3 = Point3D()):
-        Segment.__init__(self, p1, p2)
+class triangle(segment):
+    def __init__(self, p1 = point3d(), p2 = point3d(), p3 = point3d()):
+        segment.__init__(self, p1, p2)
         self.p3 = p3
     def __str__(self):
-        return Segment.__str__(self)[:-1] + " ; {}]".format(str(self.p3))
+        return segment.__str__(self)[:-1] + " ; {}]".format(str(self.p3))
 
-def parseStl(fileName):
+def parse_stl(fileName):
 	f = open(fileName, "r")
 	s = f.read()
 	l = s.split()
 	
-	listTriangle = []
+	listtriangle = []
 	i = 0
-	i = parseBeginSolid(l, i)
+	i = parse_begin_solid(l, i)
 
 	while l[i] == "facet":
-		i, t = parseFacet(l, i)
-		listTriangle.append(t)
+		i, t = parse_facet(l, i)
+		listtriangle.append(t)
 
-	i = parseEndSolid(l, i)
+	i = parse_end_solid(l, i)
 	f.close()
 
-	return listTriangle
+	return listtriangle
 
-def parseFacet(l, i):
-		i = parseBeginFacet(l, i)
-		i = parseBeginLoop(l, i)
-		i, t = parseTriangle(l, i)
-		i = parseEndLoop(l, i)
-		i = parseEndFacet(l, i)
+def parse_facet(l, i):
+		i = parse_begin_facet(l, i)
+		i = parse_begin_loop(l, i)
+		i, t = parse_triangle(l, i)
+		i = parse_end_loop(l, i)
+		i = parse_end_facet(l, i)
 
 		return i, t
 
@@ -59,42 +59,42 @@ def parse(l, i, string):
 	i += 1
 	return i
 
-def parseBeginSolid(l, i):
+def parse_begin_solid(l, i):
 	i = parse(l, i, "solid")
 	i += 1
 	return i
 
-def parseEndSolid(l, i):
+def parse_end_solid(l, i):
 	i = parse(l, i, "endsolid")
 	i += 1
 	return i
 	
-def parseEndLoop(l, i):
+def parse_end_loop(l, i):
 	i = parse(l, i, "endloop")
 	return i
 	
-def parseBeginFacet(l, i):
+def parse_begin_facet(l, i):
 	i = parse(l, i, "facet")
 	i = parse(l, i, "normal")
 
 	i += 3
 	return i
 
-def parseEndFacet(l, i):
+def parse_end_facet(l, i):
 	i = parse(l, i, "endfacet")
 	return i
 
-def parseBeginLoop(l, i):
+def parse_begin_loop(l, i):
 	i = parse(l, i, "outerloop")
 	return i
 
-def parseEndLoop(l, i):
+def parse_end_loop(l, i):
 	i = parse(l, i, "endloop")
 	return i
 	
-def parsePoint(l, i):
+def parse_point(l, i):
 	i = parse(l, i, "vertex")
-	p = Point3D()
+	p = point3d()
 	p.x = float(l[i])
 	i += 1
 	p.y = float(l[i])
@@ -103,9 +103,9 @@ def parsePoint(l, i):
 	i += 1
 	return i, p
 
-def parseTriangle(l, i):
-	t = Triangle()
-	i, t.p1 = parsePoint(l, i)
-	i, t.p2 = parsePoint(l, i)
-	i, t.p3 = parsePoint(l, i)
+def parse_triangle(l, i):
+	t = triangle()
+	i, t.p1 = parse_point(l, i)
+	i, t.p2 = parse_point(l, i)
+	i, t.p3 = parse_point(l, i)
 	return i, t
--- src/testPoint.py
+++ src/testPoint.py
@@ -1,28 +1,28 @@
 import point
 
-p2 = point.Point2D();
+p2 = point.point2d();
 print(str(p2))
-p2 = point.Point2D(1.0, 2.0)
+p2 = point.point2d(1.0, 2.0)
 print(str(p2))
-q2 = point.Point2D(3.0, 4.0)
+q2 = point.point2d(3.0, 4.0)
 print(str(q2))
-r2 = point.Point2D(5.0, 6.0)
+r2 = point.point2d(5.0, 6.0)
 print(str(r2))
 
-s2 = point.Segment(p2, q2)
+s2 = point.segment(p2, q2)
 print(str(s2))
 
-t2 = point.Triangle(p2, q2, r2)
+t2 = point.triangle(p2, q2, r2)
 print(str(t2))
 
-p3 = point.Point3D();
+p3 = point.point3d();
 print(str(p3))
-p3 = point.Point3D(1.0, 2.0, 3.0)
+p3 = point.point3d(1.0, 2.0, 3.0)
 print(str(p3))
-q3 = point.Point3D(4.0, 5.0, 6.0)
+q3 = point.point3d(4.0, 5.0, 6.0)
 print(str(q3))
 
-s3 = point.Segment(p3, q3)
+s3 = point.segment(p3, q3)
 print(str(s3))
 
 s = "ceci est une\nligne de texte"
@@ -36,9 +36,9 @@ if mot == "bonjour":
 if mot != "Bonjour":
 	print("ok == str")
 
-listTr = point.parseStl("solid.stl")
+listTr = point.parse_stl("solid.stl")
 print(str(listTr[0]))
 print(str(listTr[1]))
 
-t = point.Triangle()
+t = point.triangle()
 print(str(t))
