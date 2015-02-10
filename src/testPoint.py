54aef34df1af1b5fa685b0b6f63d0367e3c01295
Pierre Petitbon <pierre.petitbon@ensimag.grenoble-inp.fr>
2015-02-10 01:02:35 +0100

    modif test point, segment, triangle

--- src/testPoint.py
+++ src/testPoint.py
@@ -6,11 +6,21 @@ p2 = point.Point2D(1.0, 2.0)
 print(str(p2))
 q2 = point.Point2D(3.0, 4.0)
 print(str(q2))
+r2 = point.Point2D(5.0, 6.0)
+print(str(r2))
 
 s2 = point.Segment(p2, q2)
+print(str(s2))
+
+t2 = point.Triangle(p2, q2, r2)
+print(str(t2))
 
 p3 = point.Point3D();
 print(str(p3))
 p3 = point.Point3D(1.0, 2.0, 3.0)
 print(str(p3))
+q3 = point.Point3D(4.0, 5.0, 6.0)
+print(str(q3))
 
+s3 = point.Segment(p3, q3)
+print(str(s3))
