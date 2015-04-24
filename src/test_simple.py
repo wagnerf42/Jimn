30854cfd1fc3b764a78aee8514b09870220c258c
Pierre Petitbon <pierre.petitbon@ensimag.grenoble-inp.fr>
2015-04-24 18:55:26 +0200

    simple test

---
+++ src/test_simple.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# vim : tabstop=4 expandtab shiftwidth=4 softtabstop=4
+
+from jimn.point import point
+from jimn.segment import segment
+from jimn.displayable import tycat
+from jimn.poly_builder import build_polygons
+#from jimn.poly_builder import hash_points
+#from jimn.poly_builder import print_neighbors
+
+a = point(0, 0)
+b = point(-1, 0)
+c = point(0, -1)
+d = point(1, 0)
+e = point(0, 1)
+
+ab = segment(a, b)
+bc = segment(b, c)
+ac = segment(a, c)
+cd = segment(c, d)
+ad = segment(a, d)
+de = segment(d, e)
+ea = segment(e, a)
+
+lseg = [ab, bc, ac, cd, ad, de, ea]
+tycat(lseg)
+polygons = build_polygons(lseg)
+tycat(*polygons)
+
+
