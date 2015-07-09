a94c4e447e03e40d1d799bf6840343b8c3a0f3f4
Pierre Petitbon <pierre.petitbon@gmail.com>
2015-07-09 15:34:39 +0200

    more tests

--- src/interactive_tests/test_cut_segment.py
+++ src/interactive_tests/test_cut_segment.py
@@ -19,6 +19,8 @@ segments_test.append([[0.1, 1.0], [1.1, 1.1]])
 # horizontal segment aligned on a slice center line
 segments_test.append([[0.0, 1.2], [1.2, 1.2]])
 
+segments_test.extend([[[x, -y + 1.2] for [x, y] in endpoints] for endpoints in segments_test])
+
 segments_test = [[point(c1), point(c2)] for [c1, c2] in segments_test]
 segments_test = [segment(endpoints) for endpoints in segments_test]
 
@@ -26,5 +28,4 @@ heights = [0.3 * n for n in range(0, 5)]
 center_lines = [segment([point([0.0, h]), point([1.2, h])]) for h in heights]
 for s in segments_test:
     elementary_segments = s.cut(0.3)
-    print([str(e) for e in elementary_segments])
     tycat(center_lines, *elementary_segments)
