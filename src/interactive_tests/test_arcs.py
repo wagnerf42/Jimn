b3dd027e096ab6e7632ba8c4314f227e3a20d145
frederic wagner <frederic.wagner@imag.fr>
2015-07-16 13:10:58 +0200

    bugfix in arcs intersection

--- src/interactive_tests/test_arcs.py
+++ src/interactive_tests/test_arcs.py
@@ -5,36 +5,40 @@ from jimn.arc import arc
 from jimn.displayable import tycat
 from jimn.coordinates_hash import coordinates_hash
 
+labels = (
+    "testing with double intersections",
+    "testing with single intersection",
+    "testing with no intersection (too far)",
+    "testing with no intersection (same center)",
+    "testing with no intersection (but close enough)",
+)
+
 a1 = arc(
     3, [point([0, 0]), point([3, 3])]
 )
 
-a2 = arc(
-    3, [point([1, 2]), point([4, -1])]
+arcs = (
+    arc(
+        3, [point([1, 2]), point([4, -1])]
+    ),
+    arc(
+        3, [point([3, 3]), point([6, 0])]
+    ),
+    arc(
+        3, [point([4, 3]), point([7, 0])]
+    ),
+    arc(
+        2, [point([0, 1]), point([2, 3])]
+    ),
+    arc(
+        3, [point([1, -0.5]), point([4, -3.5])]
+    )
 )
 
-a3 = arc(
-    3, [point([3, 3]), point([6, 0])]
-)
-
-a4 = arc(
-    3, [point([4, 3]), point([7, 0])]
-)
-
-a5 = arc(
-    2, [point([0, 1]), point([2, 3])]
-)
 
 rounder = coordinates_hash(2)
-print("testing with double intersections")
-i = a1.intersection_with_arc(a2, rounder)
-tycat(a1, a2, i)
-print("testing with single intersection")
-i = a1.intersection_with_arc(a3, rounder)
-tycat(a1, a3, i)
-print("testing with no intersection (too far)")
-i = a1.intersection_with_arc(a4, rounder)
-tycat(a1, a4, i)
-print("testing with no intersection (same center)")
-i = a1.intersection_with_arc(a5, rounder)
-tycat(a1, a5, i)
+
+for i, a in enumerate(arcs):
+    print(labels[i])
+    intersection = a1.intersection_with_arc(a, rounder)
+    tycat(a1, a, intersection)
