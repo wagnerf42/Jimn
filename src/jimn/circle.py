9f53ba33c93441cfae49b5ca90c2845f7d83ae1d
frederic wagner <frederic.wagner@imag.fr>
2015-11-06 10:51:48 +0100

    circle type

---
+++ src/jimn/circle.py
@@ -0,0 +1,27 @@
+class circle:
+    def __init__(self, center, radius):
+        self.center = center
+        self.radius = radius
+
+    def save_svg_content(self, display, color):
+        svg_coordinates = display.convert_coordinates(
+            self.center.get_coordinates()
+        )
+        display.write("<circle cx=\"{}\" cy=\"{}\"".format(*svg_coordinates))
+        display.write(" r=\"{}\" fill=\"none\" stroke=\"{}\"".format(
+            self.radius * display.stretch(), color
+        ))
+        display.write(" opacity=\"0.5\" stroke-width=\"{}\"/>\n".format(
+            display.stroke_width()
+        ))
+
+    def get_bounding_box(self):
+        min_point = self.center + point([-self.radius, -self.radius])
+        max_point = self.center + point([self.radius, self.radius])
+        return bounding_box(
+            min_point.get_coordinates(),
+            max_point.get_coordinates(),
+        )
+
+from jimn.point import point
+from jimn.bounding_box import bounding_box
