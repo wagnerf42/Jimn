1a6435d631b243bbf4674002c4454563dae6d195
frederic wagner <frederic.wagner@imag.fr>
2016-01-06 14:41:56 +0100

    updated point class to new pylint

--- src/jimn/point.py
+++ src/jimn/point.py
@@ -1,12 +1,20 @@
+"""
+points (2d or 3d) or vectors
+"""
+from math import atan2, sqrt, cos, sin, pi
+from jimn.bounding_box import bounding_box
+from jimn.utils.precision import is_almost
+
+
 class point:
     """A point is defined as a vector of any given dimension."""
 
     def __init__(self, coordinates):
-        """Inits a point with given coordinates"""
+        """init a point with given coordinates"""
         self.coordinates = coordinates
 
     def __str__(self):
-        """Prints "(x, y, ...)"."""
+        """print code generating the point"""
         return "point([" + ', '.join([str(c) for c in self.coordinates]) + "])"
 
     def squared_distance_to(self, other):
@@ -23,26 +31,32 @@ class point:
         return len(self.coordinates)
 
     def get_coordinates(self):
-        """Return the coordinates' list of the point. Allow modifications."""
+        """return the coordinates' list of the point."""
         return self.coordinates
 
     def get_x(self):
-        """Return the first coordinate, assuming it exists."""
+        """return the first coordinate, assuming it exists."""
         return self.coordinates[0]
 
     def get_y(self):
-        """Return the second coordinate, assuming it exists."""
+        """return the second coordinate, assuming it exists."""
         return self.coordinates[1]
 
     def get_z(self):
-        """Return the third coordinate, assuming it exists."""
+        """return the third coordinate, assuming it exists."""
         return self.coordinates[2]
 
-    def set_x(self, x):
-        self.coordinates[0] = x
+    def set_x(self, new_coordinate):
+        """
+        change x coordinate
+        """
+        self.coordinates[0] = new_coordinate
 
-    def set_y(self, y):
-        self.coordinates[1] = y
+    def set_y(self, new_coordinate):
+        """
+        change y coordinate
+        """
+        self.coordinates[1] = new_coordinate
 
     def is_above(self, height):
         """
@@ -56,17 +70,26 @@ class point:
 
     def angle_with(self, other):
         """angles are computed with respect to svg orientation"""
-        (x1, y1), (x2, y2) = [p.get_coordinates() for p in (self, other)]
-        raw_angle = -atan2(y2 - y1, x2 - x1)
+        coordinates_differences = [
+            other.coordinates[i] - self.coordinates[i]
+            for i in (1, 0)
+        ]
+        raw_angle = -atan2(*coordinates_differences)
         if raw_angle <= 0:
             raw_angle += 2*pi
         return raw_angle
 
     def get_bounding_box(self):
+        """
+        return min bounding box containing point.
+        this method is defined on any displayable object.
+        """
         return bounding_box(self.coordinates, self.coordinates)
 
-    # careful : only works on 2d points
     def save_svg_content(self, display, color):
+        """
+        svg display for tycat
+        """
         svg_coordinates = display.convert_coordinates(self.coordinates)
         stroke_width = display.stroke_width()
         display.write("<circle cx=\"{}\" cy=\"{}\"".format(*svg_coordinates))
@@ -77,36 +100,45 @@ class point:
         """rotates a point around origin"""
         if __debug__:
             assert self.dimension() == 2, "2d rotation only"
-        x, y = self.coordinates
-        c = cos(-angle)
-        s = sin(-angle)
+        cosinus = cos(-angle)
+        sinus = sin(-angle)
         return point([
-            c*x - s*y,
-            s*x + c*y
+            self.scalar_product(point([cosinus, -sinus])),
+            self.scalar_product(point([sinus, cosinus]))
         ])
 
     def rotate_around(self, center, angle):
         """rotates a point around another one"""
         return center + (self-center).rotate(angle)
 
-    def is_aligned_with(self, p2, p3):
-        (x1, y1), (x2, y2), (x3, y3) = [
-            p.get_coordinates() for p in (self, p2, p3)
+    def is_aligned_with(self, point2, point3):
+        """
+        are the three given points approximately aligned ?
+        """
+        (x_1, y_1), (x_2, y_2), (x_3, y_3) = [
+            p.get_coordinates() for p in (self, point2, point3)
         ]
-        determinant = x1*y2 + y1*x3 + x2*y3 - (y2*x3 + y1*x2 + x1*y3)
+        determinant = x_1*y_2 + y_1*x_3 + x_2*y_3 \
+            - (y_2*x_3 + y_1*x_2 + x_1*y_3)
         return abs(determinant) < 10**-5  # TODO: why 5 ???
 
     def is_near(self, other, limit):
+        """
+        return true if distance from self to other
+        is less than limit
+        """
         distance = self.distance_to(other)
         if is_almost(distance, limit):
             return True
         return distance < limit
 
     def is_almost(self, other):
-        assert self.dimension() == other.dimension(), \
-            "comparing different points"
-        for u1, u2 in zip(self.coordinates, other.coordinates):
-            if not is_almost(u1, u2):
+        """
+        return true if self and other are almost the same points
+        (see jimn.utils.precision)
+        """
+        for c_1, c_2 in zip(self.coordinates, other.coordinates):
+            if not is_almost(c_1, c_2):
                 return False
         return True
 
@@ -115,24 +147,22 @@ class point:
         Returns the projection of the point on the first 2 dimensions.
         Assumes the point has at least 2 dimensions.
         """
-        x, y = self.coordinates[0:2]
-        return point([x, y])
+        return point(self.coordinates[0:2])
 
     def scalar_product(self, other):
-        p = 0
-        for c1, c2 in zip(self.coordinates, other.coordinates):
-            p += c1 * c2
-        return p
+        """
+        scalar (dot) product between two vectors
+        """
+        return sum([c1*c2
+                    for c1, c2 in zip(self.coordinates, other.coordinates)])
 
     def cross_product(self, other):
-        assert self.dimension() == 2
-        x1, y1 = self.get_coordinates()
-        x2, y2 = other.get_coordinates()
-        return x1 * y2 - y1 * x2
-
-    def perpendicular_vector(self):
-        assert self.dimension() == 2
-        return point([self.get_y(), -self.get_x()])
+        """
+        cross product between 2 2d vectors
+        """
+        x_1, y_1 = self.get_coordinates()
+        x_2, y_2 = other.get_coordinates()
+        return x_1 * y_2 - y_1 * x_2
 
     def nearest_points(self, other):
         """
@@ -141,33 +171,44 @@ class point:
         """
         return (self, other)
 
-    def __add__(a, b):
-        return point([i + j for i, j in zip(a.coordinates, b.coordinates)])
+    def __add__(self, other):
+        """
+        addition operator. (useful for translations)
+        """
+        return point([i + j
+                      for i, j in zip(self.coordinates, other.coordinates)])
 
-    def __sub__(a, b):
-        return point([i - j for i, j in zip(a.coordinates, b.coordinates)])
+    def __sub__(self, other):
+        """
+        substraction operator. (useful for translations)
+        """
+        return point([i - j
+                      for i, j in zip(self.coordinates, other.coordinates)])
 
-    def __mul__(a, i):
-        return point([c*i for c in a.coordinates])
+    def __mul__(self, factor):
+        """
+        multiplication by scalar operator. (useful for scaling)
+        """
+        return point([c*factor for c in self.coordinates])
 
-    def __truediv__(a, i):
-        return point([c/i for c in a.coordinates])
+    def __truediv__(self, factor):
+        """
+        division by scalar operator. (useful for scaling)
+        """
+        return point([c/factor for c in self.coordinates])
 
-    def __eq__(a, b):
-        return a.coordinates == b.coordinates
+    def __eq__(self, other):
+        """
+        strict equality operator
+        """
+        return self.coordinates == other.coordinates
 
     def __hash__(self):
         return hash(tuple(self.coordinates))
 
-    def __lt__(a, b):
+    def __lt__(self, other):
         """
         Returns whether or not a < b in lexicographical order.
         Assumes a and b have the same dimension.
         """
-        return a.coordinates < b.coordinates
-
-
-
-from math import atan2, sqrt, cos, sin, pi
-from jimn.bounding_box import bounding_box
-from jimn.utils.precision import is_almost
+        return self.coordinates < other.coordinates
