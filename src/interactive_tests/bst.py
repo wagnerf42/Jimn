cf428d20b97dd007b1a0be94a46883394466bee0
frederic wagner <frederic.wagner@imag.fr>
2016-03-04 11:47:29 +0100

    treap seems ok now

--- src/interactive_tests/bst.py
+++ src/interactive_tests/bst.py
@@ -4,7 +4,7 @@ from time import clock
 from random import random, seed, shuffle
 from jimn.tree.treap import Treap
 
-root = Treap(-1, priority=1000)
+root = Treap(-1, root_node=True)
 seconds = clock()
 print(seconds)
 seed(seconds)
@@ -18,7 +18,13 @@ for _ in range(10):
     root.add(new_value)
     root.tycat()
 
+print("neighbours")
+for value in sorted(values):
+    neighbours = [n.content for n in root.find(value).neighbours()]
+    print(value, "is neighboured by", [str(v) for v in neighbours])
+
 print("destruction")
+shuffle(values)
 for value in values:
     print("removing:", value)
     node = root.find(value)
--- src/jimn/tree/treap.py
+++ src/jimn/tree/treap.py
@@ -5,17 +5,20 @@ providing treap class.
 from random import random
 from jimn.tree import Tree
 
+MAX_PRIORITY = 2
+
 
 class Treap(Tree):
     """
     self-balancing BST.
     """
-    def __init__(self, content, father=None, priority=None):
+    # pylint: disable=protected-access
+    def __init__(self, content, root_node=False):
         super().__init__(content)
         self.children = [None, None]
-        self.father = father
-        if priority is not None:
-            self.priority = priority
+        self.father = None  # for now
+        if root_node:
+            self.priority = MAX_PRIORITY
         else:
             self.priority = random()
 
@@ -24,12 +27,14 @@ class Treap(Tree):
         add given content in tree.
         """
         direction = (self.content < content)
-        if self.children[direction]:
-            self.children[direction].add(content)
-        else:
-            new_child = Treap(content, father=self)
-            self.children[direction] = new_child
-            new_child.balance()
+        node = self
+        while node.children[direction]:
+            node = node.children[direction]
+            direction = (node.content < content)
+
+        new_child = Treap(content)
+        node._set_child(direction, new_child)
+        new_child._balance()
 
     def remove(self):
         """
@@ -38,13 +43,13 @@ class Treap(Tree):
         # first easy cases : one or zero children
         father = self.father
         if self.children[True] is None:
-            father.set_child(father.direction_to(self), self.children[False])
+            father._set_child(father._direction_to(self), self.children[False])
         elif self.children[False] is None:
-            father.set_child(father.direction_to(self), self.children[True])
+            father._set_child(father._direction_to(self), self.children[True])
         else:
             # more complex case : find leftmost node in right subtree
-            extremum = self.children[True].find_extreme_node(False)
-            extremum.exchange_content_with(self)
+            extremum = self.children[True]._find_extreme_node(False)
+            extremum._exchange_content_with(self)
             extremum.remove()
 
     def find(self, content):
@@ -59,7 +64,51 @@ class Treap(Tree):
 
         return node
 
-    def find_extreme_node(self, direction):
+    def neighbours(self):
+        """
+        returns the (up to two) nodes with nearest values
+        """
+        nodes = []
+        for direction in (False, True):
+            neighbour = self._nearest_node(direction)
+            if neighbour:
+                nodes.append(neighbour)
+
+        return nodes
+
+    def dot_label(self):
+        """
+        label of given node for dot file.
+        """
+        return str(self.content) + " / " + str(self.priority)
+
+    def _is_sentinel(self):
+        """
+        are we dummy root node ?
+        """
+        return self.priority == MAX_PRIORITY
+
+    def _nearest_node(self, direction):
+        """
+        return node with content value nearest (and smaller / bigger
+        depending on direction)
+        """
+        if self.children[direction] is not None:
+            return self.children[direction]._find_extreme_node(not direction)
+        else:
+            # we need to find in ancestors
+            old = self
+            older = self.father
+            reversed_direction = not direction
+            while not older._is_sentinel():
+                if older._direction_to(old) == reversed_direction:
+                    return older
+                old = older
+                older = older.father
+
+            return None
+
+    def _find_extreme_node(self, direction):
         """
         return node in subtree the most to the given direction.
         """
@@ -68,29 +117,30 @@ class Treap(Tree):
             node = node.children[direction]
         return node
 
-    def exchange_content_with(self, other):
+    def _exchange_content_with(self, other):
         """
         exchange contents.
+        BE VERY CAREFUL. EXCHANGING CONTENT MIGHT INVALIDATE EXTERNAL POINTERS.
         """
         self.content, other.content = other.content, self.content
 
-    def direction_to(self, target_child_node):
+    def _direction_to(self, target_child_node):
         """
         which way to target child node ?
         pre-requisite : given node is one of our children.
         """
         return id(self.children[True]) == id(target_child_node)
 
-    def balance(self):
+    def _balance(self):
         """
         use priorities to balance tree starting from given node
         and going back to root node.
         """
         node = self
         while node.priority > node.father.priority:
-            node.rotate_upwards()
+            node._rotate_upwards()
 
-    def set_child(self, direction, child):
+    def _set_child(self, direction, child):
         """
         set given node as wanted child.
         """
@@ -98,23 +148,17 @@ class Treap(Tree):
         if child is not None:
             child.father = self
 
-    def rotate_upwards(self):
+    def _rotate_upwards(self):
         """
         push node upwards and father downwards in opposite direction.
         """
         father = self.father
-        direction = father.direction_to(self)
+        direction = father._direction_to(self)
         grandfather = father.father
-        grandfather_direction = grandfather.direction_to(father)
+        grandfather_direction = grandfather._direction_to(father)
         # first, replace father for grandfather
-        grandfather.set_child(grandfather_direction, self)
+        grandfather._set_child(grandfather_direction, self)
         # now exchange roles with father
         reversed_direction = not direction
-        father.set_child(direction, self.children[reversed_direction])
-        self.set_child(reversed_direction, father)
-
-    def dot_label(self):
-        """
-        label of given node for dot file.
-        """
-        return str(self.content) + " / " + str(self.priority)
+        father._set_child(direction, self.children[reversed_direction])
+        self._set_child(reversed_direction, father)
