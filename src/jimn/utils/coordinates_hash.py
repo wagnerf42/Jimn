2a6e62d610b99dcc5ecef773ae42c3d76c6cd7a2
frederic wagner <frederic.wagner@imag.fr>
2016-03-15 11:01:09 +0100

    coordinates hash work as wanted now

--- src/jimn/utils/coordinates_hash.py
+++ src/jimn/utils/coordinates_hash.py
@@ -8,10 +8,21 @@ from jimn.utils.precision import coordinate_key, displaced_coordinate_key, \
 
 class CoordinatesHash:
     """
-    hash a set of points by their rounded coordinates.
+    a CoordinatesHash is structure providing a very fast way (O(1)) of
+    merging nearby coordinates in points.
+    when initializing a new hash you need to provide the dimension of the space
+    and a wanted_precision.
+
+    we ensure that :
+        - if any two coordinates have a difference less than
+        10**-wanted_precision then they will be merged to the value of the
+        most ancient in the hash.
+        - if two coordinates are merged then IN GENERAL their difference is
+        less than 2*10**-wanted_precision (transitivity might increase this
+        value but it is very unlikely).
     """
     def __init__(self, dimension=2, wanted_precision=PRECISION):
-        self.hashes = [({}, {}) for _ in range(dimension)]
+        self.hashes = [{} for _ in range(dimension)]
         self.precision = wanted_precision
         self.fast_hash = set()  # fast test for exact match
 
@@ -23,26 +34,22 @@ class CoordinatesHash:
         """
         if point in self.fast_hash:
             return point
-        # TODO: trop moche
-        keys = [
-            [coordinate_key(c, self.precision) for c in point.coordinates],
-            [displaced_coordinate_key(c, self.precision)
-             for c in point.coordinates]
-        ]
 
         new_coordinates = list(point.coordinates)
-        for index in range(len(new_coordinates)):
+        for index, coordinate in enumerate(new_coordinates):
+            key = coordinate_key(coordinate, self.precision)
+            displaced_key = displaced_coordinate_key(coordinate,
+                                                     self.precision)
             # update
-            for hash_index, coordinates_hash in enumerate(self.hashes[index]):
-                key = keys[hash_index][index]
-                if key in coordinates_hash:
-                    new_coordinates[index] = coordinates_hash[key]
-                    break
-
-            # re-hash
-            for hash_index, coordinates_hash in enumerate(self.hashes[index]):
-                key = keys[hash_index][index]
-                coordinates_hash[key] = new_coordinates[index]
+            if key in self.hashes[index]:
+                new_coordinates[index] = self.hashes[index][key]
+                self.hashes[index][displaced_key] = new_coordinates[index]
+            elif displaced_key in self.hashes[index]:
+                new_coordinates[index] = self.hashes[index][displaced_key]
+                self.hashes[index][key] = new_coordinates[index]
+            else:
+                self.hashes[index][key] = new_coordinates[index]
+                self.hashes[index][displaced_key] = new_coordinates[index]
 
         hashed_point = Point(new_coordinates)
         self.fast_hash.add(hashed_point)
--- src/jimn/utils/precision.py
+++ src/jimn/utils/precision.py
@@ -5,7 +5,7 @@ this stuff is highly toxic so take care.
 
 PRECISION = 6
 LIMIT = 10**-PRECISION
-SEGMENT_LIMIT = LIMIT # minimum length allowed for a segment
+SEGMENT_LIMIT = LIMIT  # minimum length allowed for a segment
 PRECISION_FORMAT = "{{0:.{}f}}".format(PRECISION)
 
 
@@ -36,6 +36,7 @@ def displaced_coordinate_key(coordinate, wanted_precision=PRECISION):
     return string display of given coordinate
     displaced by half precision.
     """
+    # TODO: just add 0.000000....01 to key ??
     wanted_limit = 10**-wanted_precision
     return coordinate_key(coordinate + wanted_limit, wanted_precision)
 
@@ -44,9 +45,10 @@ def print_debug(coordinate):
     """
     display coordinate with high precision and keys.
     """
-    print('c: {0:.10f}'.format(coordinate))
-    print(coordinate_key(coordinate))
-    print(displaced_coordinate_key(coordinate))
+    print("raw coordinate", coordinate)
+    print('10 decimal places : {0:.10f}'.format(coordinate))
+    print("key", coordinate_key(coordinate))
+    print("displaced key", displaced_coordinate_key(coordinate))
 
 
 def check_precision(coordinate1, coordinate2, debug_message):
@@ -56,5 +58,7 @@ def check_precision(coordinate1, coordinate2, debug_message):
     if is_almost(coordinate1, coordinate2):
         print('warning: potential precision problem : ', debug_message)
         print('c1: {0:.10f} c2: {1:.10f}'.format(coordinate1, coordinate2))
+        print("diff:", abs(coordinate2-coordinate1), "limit:", LIMIT)
         print_debug(coordinate1)
         print_debug(coordinate2)
+        raise Exception("prec")
