804eb1c8f9d78e284f90c32c3c97ec551174784c
frederic wagner <frederic.wagner@imag.fr>
2016-11-17 09:37:49 +0100

    haskell : stl parsing works

--- haskell/jimn/src/Model.hs
+++ haskell/jimn/src/Model.hs
@@ -2,19 +2,34 @@ module Model( Model(..)
             , load
             ) where
 
+import Point
 import Facet
 import qualified Data.ByteString.Lazy as B
 import Data.Binary.Get
 import Debug.Trace
+import GHC.Float
+import Control.Monad
 
 data Model = Model [Facet] deriving(Show)
 
-parser :: Get [Facet]
-parser = do
-  a <- getFloatle
-  return []
+parseFacets :: Get [Facet]
+parseFacets = do
+  facetsNumber <- getInt32le
+  forM [1..facetsNumber] (const parseFacet)
+
+parseFacet :: Get Facet
+parseFacet = do
+  normal <- sequence [getFloatle, getFloatle, getFloatle]
+  c1 <- sequence [getFloatle, getFloatle, getFloatle]
+  c2 <- sequence [getFloatle, getFloatle, getFloatle]
+  c3 <- sequence [getFloatle, getFloatle, getFloatle]
+  let coordinates = [c1, c2, c3]
+      doubleCoordinates = map (map float2Double) coordinates
+      [p1, p2, p3] = map Point doubleCoordinates in
+      return $ Facet p1 p2 p3
+
 
 load :: FilePath -> IO Model
 load filename = do
   contents <- B.readFile filename
-  return $ Model $ runGet parser (B.drop 84 contents)
+  return $ Model $ runGet parseFacets (B.drop 80 contents)
